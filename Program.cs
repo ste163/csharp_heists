9c2cf01f5f7be22cb8c50c2b555efbaaa925edfc
ste163 <samtedwards@pm.me>
2020-11-20 15:12:47 -0500

    able to hire criminals, refactored code

--- Program.cs
+++ Program.cs
@@ -7,33 +7,98 @@ namespace heist
         static void Main(string[] args)
         {
             Console.WriteLine("Plan Your Heist!");
+            Member Terra = HireNewCriminal();
+            Member Tristan = HireNewCriminal();
+            Member Sam = HireNewCriminal();
+  
+            // Display team member's info
+
+        }
+
+        static Member HireNewCriminal()
+        {
+            Console.Write("Enter new team member's name: ");
+            // User can enter any string, including a string of numbers
+            string enteredName = Console.ReadLine();
+            int enteredSkill = InputSkill();
+            double enteredCourage = InputCourage();
+            Member newCriminal = new Member(enteredName, enteredSkill, enteredCourage);
+
+            Console.WriteLine($@"
+{newCriminal.Name} hired!
+Skill level: {newCriminal.SkillLevel}
+Courage Factor: {newCriminal.CourageFactor}
+");
+
+            return newCriminal;
+        }
+
+        static int InputSkill()
+        {
+            int entered;
 
             while(true)
             {
                 try
                 {
-                    Console.Write("Enter new team member's name: ");
-                    // User can enter any string, including a string of numbers
-                    string enteredName = Console.ReadLine();
-
-                    // User can still enter a negative number
                     Console.Write("Enter new team member's skill level: ");
-                    int enteredSkill = int.Parse(Console.ReadLine());
-
-                    // User can still enter ANY number outside the range
-                    Console.Write("Enter new team member's courage (0.0 - 2.0): ");
-                    double enteredCourage = int.Parse(Console.ReadLine());
-
-                    Member newTeamMember = new Member(enteredName, enteredSkill, enteredCourage);
+                    entered = int.Parse(Console.ReadLine());
                     break;
                 }
-                catch(FormatException)
+                catch
                 {
-                    Console.WriteLine("You must enter a number.");
+                    Console.WriteLine("Must enter a whole number.");
                 }
             }
 
-            // Display team member's info
+            while(entered <= 0)
+            {
+                try
+                {
+                    Console.Write("Enter whole number greater than zero: ");
+                    entered = int.Parse(Console.ReadLine());
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Must enter a whole number.");
+                }
+            }
+
+            return entered;
+        }
+
+        static double InputCourage()
+        {
+            double entered;
+
+            while(true)
+            {
+                try
+                {
+                    Console.Write("Enter new team member's courage (0.0 - 2.0): ");
+                    entered = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Must enter number.");
+                }
+            }
+
+            while(entered < 0.0 || entered > 2.0)
+            {
+                try
+                {
+                    Console.Write("Enter number between 0.0 - 2.0: ");
+                    entered = double.Parse(Console.ReadLine());
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Must enter a number.");
+                }
+            }
+
+            return entered;
         }
     }
 }
