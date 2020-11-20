6bc8e9deff407aedfde0c0de7a87a2e1eca51791
ste163 <samtedwards@pm.me>
2020-11-20 13:00:18 -0500

    able to create a new team member and catch exceptions

--- Program.cs
+++ Program.cs
@@ -6,11 +6,33 @@ namespace heist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Plan Your Hesit!");
+            Console.WriteLine("Plan Your Heist!");
 
-            // Prompt user to enter a team member name and save name
-            // Prompt user to enter team member's skill level and save skill level to that member
-            // Prompt user to enter team member's courage and save to that member
+            while(true)
+            {
+                try
+                {
+                    Console.Write("Enter new team member's name: ");
+                    // User can enter any string, including a string of numbers
+                    string enteredName = Console.ReadLine();
+
+                    Console.Write("Enter new team member's skill level: ");
+                    int enteredSkill = int.Parse(Console.ReadLine());
+
+                    Console.Write("Enter new team member's courage (0.0 - 2.0): ");
+                    double enteredCourage = int.Parse(Console.ReadLine());
+
+                    Member newTeamMember = new Member(enteredName, enteredSkill, enteredCourage);
+                    break;
+                }
+                // Have specific catches for
+                // each error message based on
+                // which variable we are declaring & assigning
+                catch(FormatException)
+                {
+                    Console.WriteLine("You must enter a number");
+                }
+            }
 
             // Display team member's info
         }
