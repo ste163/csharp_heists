03594a165e854b81648f813fcbc9797662aee1de
ste163 <samtedwards@pm.me>
2020-11-21 11:02:44 -0500

    added new intro text, trust level to criminal

--- Criminal.cs
+++ Criminal.cs
@@ -5,10 +5,13 @@ namespace heist
     public class Criminal
     {
         public string Name { get; set; }
-        // Skill level must be positive
+        // Skill level must be 1 - 100
         public int SkillLevel { get; set; }
+
+        // 1 -100
+        public int Trust { get; set; }
         
-        // Courage Factor must be 0.0 - 2.0
+        // Courage Factor must be 0.1 - 1.0
         public double CourageFactor { get; set; }
 
         public Criminal(string name,int skill, double courage)
