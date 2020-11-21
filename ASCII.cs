8e7fe3a20993d79f2215716dd2f60e519474015c
ste163 <samtedwards@pm.me>
2020-11-20 19:00:35 -0500

    found ASCII art for faces, money, and jailed

--- ASCII.cs
+++ ASCII.cs
@@ -4,6 +4,129 @@ namespace heist
 {
     public class ASCII
     {
-        
+        public void DisplayCriminalFace()
+        {
+            // Create a random number
+            // If the random is ???
+            // Then show the matching face
+            Console.WriteLine(@"");
+        }
+
+        public void DisplayMoney()
+        {
+            Console.WriteLine(@"
+--------------------------------------------------------------------.
+| .--                    FEDERAL RESERVE NOTE                    .-- |
+| |_       ......    THE UNTIED STATES OF AMERICA                |_  |
+| __)    ``````````             ______            B93810455B     __) |
+|      2        ___            /      \                     2        |
+|              /|~\\          /  _-\\  \           __ _ _ _  __      |
+|             | |-< |        |  //   \  |         |_  | | | |_       |
+|              \|_//         | |-  o o| |         |   | `.' |__      |
+|               ~~~          | |\   b.' |                            |
+|       B83910455B           |  \ '~~|  |                            |
+| .--  2                      \_/ ```__/    ....            2    .-- |
+| |_        ///// ///// ////   \__\'`\/      ``  //// / ////     |_  |
+| __)                   M A J O R  D O L L A R S                   __) |
+`--------------------------------------------------------------------'
+");
+// Thank you for visiting https://asciiart.website/
+// This ASCII pic can be found at
+// https://asciiart.website/index.php?art=objects/money
+
+        }
+
+        public void DisplayJailed()
+        {
+            Console.WriteLine(@"
+================================
+||     ||<(.)>||<(.)>||     || 
+||    _||     ||     ||_    || 
+||   (__D     ||     C__)   || 
+||   (__D     ||     C__)   ||
+||   (__D     ||     C__)   ||
+||   (__D     ||     C__)   ||
+||     ||     ||     ||     ||
+================================    
+");
+        }
     }
 }
+
+ \\\\\\\|
+ | _   _ |       
+( (o) (o) )     
+ |  . .  |           
+  \  _  /         
+   \___/
+
+   |||||   
+ {. @ @ .} 
+  |  o  |        
+   \_U_/
+
+  .xxxx. 
+ |(o)(o)|
+(  (__)  )
+  | __ |
+   \__/ 
+        
+
+     _____
+    /     `
+   /`---'- \
+  (_    _ \_\
+  `o'/ 'o` 7)
+   \`-     |
+    \`~'  /\
+     `---'  \___
+     --`_---'   
+
+     ______
+  ,-'      `.
+ /           \
+ | __         |
+ \   \__  -.  |
+  ( o( o) 7/ /
+ /  /     -|/
+ \ (_     / |
+  \ --.  /  |
+  (_____/   |
+
+
+
+       .-"```'.
+      /   \    \
+     /   / `\__/
+     | .'  _  _|
+     \(\   6  6
+      | \   _\ |
+      |\ `~`= `/
+      | '.___.'
+  .'` \     |_
+        '-__ / `-
+
+          _.===.
+       _,/_ \\\ `-.
+      /`/| /      _\
+     // ` /////////\
+    | /`|/=*===*=| (
+   /`/` /|<o>)(o>(  )
+   \  \/\|    /  /\/
+   \`/` \/\ `=' /
+   |/\/` \/`._.'
+   /` |/`/ |  '
+   \//| /
+    ' '/
+
+
+
+
+
+
+
+// Thank you for visiting https://asciiart.website/
+// This ASCII pic can be found at
+// https://asciiart.website/index.php?art=people/faces
+
+
